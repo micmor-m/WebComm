4de7466ddc3d69614b3feac0b323b509b624a0c8
mic <micmor.m@gmail.com>
2020-12-09 14:07:30 -0500

    implement basic logic to send a POST request with new message

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,52 @@
-//to be able to commnicate with the server
-const xhr = new XMLHttpRequest();
-
-//the xhr obj takes 2 params. The method and the URL
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
-
-xhr.responseType = "json";
-
-//1 select the ul element where to add the posts
 const listElement = document.querySelector(".posts");
-// 2 select the template to inject a single post
 const postTemplate = document.getElementById("single-post");
 
-//the onload function is async
-xhr.onload = function () {
-  //const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
+function sendHttpRequest(method, url, data) {
+  //create a Promise to handle the data from the server
+  const promise = new Promise((resolve, reject) => {
+    //commnicate with the server
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
 
-  //now that we have all posts we need to render them in the DOM
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    listElement.append(postEl);
-  }
-};
+    //the onload function is async
+    xhr.onload = function () {
+      resolve(xhr.response);
+      //const listOfPosts = JSON.parse(xhr.response);
+    };
 
-xhr.send();
+    //send the request to te server
+    //if data in not undefined, the data will be append to thhe request
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
+}
+
+function fetchPosts() {
+  sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
+    (responseData) => {
+      const listOfPosts = responseData;
+      //now that we have all posts we need to render them in the DOM
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("p").textContent = post.body;
+        listElement.append(postEl);
+      }
+    }
+  );
+}
+
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchPosts();
+createPost("DUMMY", "A dummy post!");
